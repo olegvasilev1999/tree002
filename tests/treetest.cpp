c4157b02331200c171082b51cd96df207d276314
olegvasilev1999 <32066946+olegvasilev1999@users.noreply.github.com>
2018-04-27 03:31:31 +0600

    Update treetest.cpp

--- tests/treetest.cpp
+++ tests/treetest.cpp
@@ -1,91 +1,34 @@
 #include <catch.hpp>
-#include "tree.cpp"
+#include <sstream>
 
-TEST_CASE("insert and remove")
-{
-  std::string input1{
-      "------------14\n"
-      "--------12\n"
-      "------------11\n"
-      "----10\n"
-      "------------9\n"
-      "--------7\n"
-      "------------6\n"
-      "--------------------4\n"
-      "----------------3\n"};
-  std::string input2{
-      "------------14\n"
-      "--------12\n"
-      "------------11\n"
-      "----10\n"
-      "------------9\n"
-      "--------7\n"
-      "-----------------4\n"
-      "-------------3\n"};
-  tree_t<int> Tree;
-  
-  Tree.insert(10);
-  Tree.insert(12);
-  Tree.insert(11);
-  Tree.insert(14);
-  Tree.insert(7);
-  Tree.insert(9);
-  Tree.insert(6);
-  Tree.insert(3);
-  Tree.insert(4);
-  
-  std::ostringstream ostream;
-  Tree.print( ostream );
-  
-  REQUIRE( input1 == ostream.str() );
-  
-  REQUIRE(Tree.remove(6) == true);
-  Tree.print( ostream );
-  
-  REQUIRE( input1 + input2 == ostream.str() );
+#include "tree_class.cpp"
+
+TEST_CASE("insert,print and remove")
+{   std::string input{"------------18\n----------------17\n--------16\n------------13\n----10\n--------4\n----------------3\n------------2\n"};
+std::string input1{"------------18\n----------------17\n--------16\n----13\n--------4\n----------------3\n------------2\n"};
+     std::ostringstream ostream;
+    tree_t<int> tree{10,4,16,2,13,18,3,17};
+    tree.print(ostream);
+    std::cout<<"removing 10:"<<(tree.remove(10)? "removed":"not removed")<<'\n';
+    tree.print(ostream);
+    REQUIRE(input+input1==ostream.str());
+   
+}
+TEST_CASE("compare"){
+
+
+
+    std::ostringstream ostream;
+    tree_t<int> tree{10,4,16,2,13,18,3,17};
+    tree_t<int> tree1{10,4,16,2,13,18,3,17};
+    ostream<<(tree==tree1?"equal\n":"not equal\n");
+    tree1.insert(8);
+    tree1.insert(9);
+    tree1.insert(15);
+    ostream<<(tree==tree1?"equal\n":"not equal\n");
+    ostream<<(tree.find(13)? "found succesfully\n":"not found\n");
+    REQUIRE("equal\nnot equal\nfound succesfully\n"==ostream.str());
+
 }
 
-
-TEST_CASE("initializer_list")
-{
-   std::string input{
-      "------------14\n"
-      "--------12\n"
-      "------------11\n"
-      "----10\n"
-      "------------9\n"
-      "--------7\n"
-      "------------6\n"
-      "--------------------4\n"
-      "----------------3\n"};
-  tree_t<int> Tree{10,12,11,14,7,9,6,3,4};
-  
-  std::ostringstream ostream;
-  Tree.print( ostream );
-  
-  REQUIRE( input == ostream.str() );
-  
-  
-}
-
-
-TEST_CASE("operator ==")
-{
-  tree_t<int> Tree1{10,12,11,14,7,9,6,3,4};
-  tree_t<int> Tree2{10,12,11,14,7,9,6,3,4};
-  
-  REQUIRE( (Tree1 == Tree2) == true );
-  
-  Tree2.remove(6);
-  
-  REQUIRE( (Tree1 == Tree2) == false );
-}
-
-TEST_CASE("find")
-{
-  tree_t<int> Tree{10,12,11,14,7,9,6,3,4};
-  
-  REQUIRE( (Tree.find(6)) == true );
-  REQUIRE( (Tree.find(1)) == false );
-}
 
